e903771893d9ba9c5dd2decc5ccf11f28f33cfc0
Brian McClain <brianmmcclain@gmail.com>
2012-04-24 00:16:43 -0400

    Server now accepts HTTP POST on /s to parse script, can be evaluated with cURL commands

--- server.js
+++ server.js
@@ -3,8 +3,38 @@ var Scrptr = require('./lib/scrptr').Scrptr;
 
 var PORT = process.env.VCAP_APP_PORT || 3000;
 
-function onRequest(req, res){
+var scrptr = new Scrptr();
+scrptr.addFunction("H", function(args){var date = new Date(); return date.getUTCHours();}, "Get the current hour of the time of day in UTC");
+scrptr.addFunction("M", function(args){var date = new Date(); return date.getUTCMinutes();}, "Get the current minute of the time of day in UTC");
+scrptr.addFunction("AMPM", function(args){var date = new Date(); if (date.getUTCHours() > 11){return "PM"} else {return "AM"}}, "Determines if the current time is in AM or PM in UTC");
+scrptr.addFunction("TEMP", function(args){return "100"}, "Get the current temperature in fahrenheit for {AREA_CODE}");
+
+function onScriptPOST(req, res){
 	
+	buffer = "";
+	
+	req.on('data', function(chunk){
+		if (chunk != undefined){
+			buffer += chunk.toString();
+		}
+	});
+	
+	req.on('end', function(){
+		scrptr.eval(buffer, function(error, data){
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			if (error){
+				res.write(error);
+			}
+			else{
+				res.write(data);
+			}
+			
+			res.end();
+		});		
+	});
+}
+
+function onRequest(req, res){
 	switch(req.url){
 		case '/' :
 			res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -12,6 +42,20 @@ function onRequest(req, res){
 			res.end();
 			break;
 		case '/s' :
+			if (req.method == "POST"){
+				// Handle POST request, this is an API request
+				onScriptPOST(req, res);
+			}
+			else if (req.method == "GET"){
+				res.writeHead(200, {'Content-Type': 'text/plain'});
+				res.write("Coming soon: Interactive SCRPTR");
+				res.end();
+			}
+			else{
+				res.writeHead(404, {'Content-Type': 'text/plain'});
+				res.write("Page Not Found");
+				res.end();
+			}
 			break;
 		default :
 			res.writeHead(404, {'Content-Type': 'text/plain'});
@@ -21,15 +65,3 @@ function onRequest(req, res){
 }
 
 var server = http.createServer(function(req, res) {onRequest(req, res)}).listen(PORT);
-
-var scrptr = new Scrptr();
-scrptr.addFunction("H", function(args){var date = new Date(); return date.getUTCHours();}, "Get the current hour of the time of day in UTC");
-scrptr.addFunction("M", function(args){var date = new Date(); return date.getUTCMinutes();}, "Get the current minute of the time of day in UTC");
-scrptr.addFunction("AMPM", function(args){var date = new Date(); if (date.getUTCHours() > 11){return "PM"} else {return "AM"}}, "Determines if the current time is in AM or PM in UTC");
-scrptr.addFunction("TEMP", function(args){return "100"}, "Get the current temperature in fahrenheit for {AREA_CODE}");
-
-// Sample testing code
-scrptr.eval("The time is %H %M %AMPM . It is currently %TEMP{94101} degrees Fahrenheit", function(error, text){
-	console.log(error);
-	console.log(text);
-});
