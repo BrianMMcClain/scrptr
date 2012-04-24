119436491df121efff455b881c43bf268b3b8ba5
Brian McClain <brianmmcclain@gmail.com>
2012-04-23 23:03:05 -0400

    Began work on Scrptr library

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var Scrptr = require('./lib/scrptr').Scrptr;
 
 var PORT = process.env.VCAP_APP_PORT || 3000;
 
@@ -18,3 +19,8 @@ function onRequest(req, res){
 }
 
 var server = http.createServer(function(req, res) {onRequest(req, res)}).listen(PORT);
+
+var scrptr = new Scrptr();
+scrptr.addFunction("H", function(args){var date = new Date(); return date.getHours();});
+scrptr.addFunction("H", function(args){var date = new Date(); return date.getHours();});
+scrptr.eval("The time is %H %M %AMPM");
