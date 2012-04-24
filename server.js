508a04f35f68ca93a20e5d594a0215362ad0d88a
Brian McClain <brianmmcclain@gmail.com>
2012-04-23 22:22:02 -0400

    Pulled request event for http server to seperate function, added 404 page

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ var http = require('http');
 
 var PORT = process.env.VCAP_APP_PORT || 3000;
 
-var server = http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write("Welcome to SCRPTR");
-	res.end();
-}).listen(PORT);
+function onRequest(req, res){
+	
+	if (req.url == "/"){
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.write("Welcome to SCRPTR");
+		res.end();
+	}
+	else{
+		res.writeHead(404, {'Content-Type': 'text/plain'});
+		res.write("Page Not Found");
+		res.end();
+	}
+	
+}
+
+var server = http.createServer(function(req, res) {onRequest(req, res)}).listen(PORT);
