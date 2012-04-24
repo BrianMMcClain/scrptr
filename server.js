984172df01ec5662009b571406941cf9f2902f5f
Brian McClain <brianmmcclain@gmail.com>
2012-04-23 23:59:11 -0400

    Cleaned up routing code

--- server.js
+++ server.js
@@ -5,17 +5,19 @@ var PORT = process.env.VCAP_APP_PORT || 3000;
 
 function onRequest(req, res){
 	
-	if (req.url == "/"){
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.write("Welcome to SCRPTR");
-		res.end();
-	}
-	else{
-		res.writeHead(404, {'Content-Type': 'text/plain'});
-		res.write("Page Not Found");
-		res.end();
-	}
-	
+	switch(req.url){
+		case '/' :
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.write("Welcome to SCRPTR");
+			res.end();
+			break;
+		case '/s' :
+			break;
+		default :
+			res.writeHead(404, {'Content-Type': 'text/plain'});
+			res.write("Page Not Found");
+			res.end();
+	}	
 }
 
 var server = http.createServer(function(req, res) {onRequest(req, res)}).listen(PORT);
