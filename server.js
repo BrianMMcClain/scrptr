58b7cb1e765e165676ca5b76c3bbc6aed30c9d4b
Brian McClain <brianmmcclain@gmail.com>
2012-04-23 23:54:34 -0400

    Scripting engine now evaluates script using function library provided by the user

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ function onRequest(req, res){
 var server = http.createServer(function(req, res) {onRequest(req, res)}).listen(PORT);
 
 var scrptr = new Scrptr();
-scrptr.addFunction("H", function(args){var date = new Date(); return date.getUTCHours();}, "Get the current hour of the time of day");
-scrptr.addFunction("M", function(args){var date = new Date(); return date.getUTCMinutes();}, "Get the current minute of the time of day");
+scrptr.addFunction("H", function(args){var date = new Date(); return date.getUTCHours();}, "Get the current hour of the time of day in UTC");
+scrptr.addFunction("M", function(args){var date = new Date(); return date.getUTCMinutes();}, "Get the current minute of the time of day in UTC");
+scrptr.addFunction("AMPM", function(args){var date = new Date(); if (date.getUTCHours() > 11){return "PM"} else {return "AM"}}, "Determines if the current time is in AM or PM in UTC");
 scrptr.addFunction("TEMP", function(args){return "100"}, "Get the current temperature in fahrenheit for {AREA_CODE}");
 
 // Sample testing code
